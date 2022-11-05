b1613590244b87ee417d1c1ab8aacb75e922e4f9
asnewman <ashznewman@gmail.com>
2022-11-04 17:17:34 -0700

    minimum bug reproduction for https://github.com/dittowords/ditto-react/issues/20

---
+++ ditto/index.js
@@ -0,0 +1,45 @@
+module.exports = {
+  "project_6275d66b8b66880129817437": {
+    "base": {
+      "frame_6275e2cf9ca86d3a458b7e5e": {
+        "frameName": "Banner",
+        "blocks": {},
+        "otherText": {
+          "banner": {
+            "text": "bimbap"
+          }
+        }
+      },
+      "frame_6275de89f175278ed82ce97d": {
+        "frameName": "Links2",
+        "blocks": {},
+        "otherText": {
+          "create_habit": {
+            "text": "Create a new habit here"
+          },
+          "view_accomp": {
+            "text": "View your accomplishments"
+          }
+        }
+      },
+      "links": {
+        "frameName": "Links",
+        "blocks": {},
+        "otherText": {
+          "log_new": {
+            "text": "Log an accomplishment"
+          },
+          "manage": {
+            "text": "Manage your habits"
+          },
+          "changelog": {
+            "text": "View changelog"
+          },
+          "report": {
+            "text": "Report bug or send feedback"
+          }
+        }
+      }
+    }
+  }
+}
